3c3518540de9d9e54cf44d8620adc3c2eae9c229
Remasterer <ronitrend@gmail.com>
2020-05-30 03:16:34 +0300

    Commit message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+import {Sort} from '@angular/material/sort';
+import {ModalDismissReasons, NgbModal} from '@ng-bootstrap/ng-bootstrap';
+
+export interface IPhoneContact {
+  firstName: string;
+  secondName: string;
+  number: string;
+}
+class PhoneContact implements IPhoneContact{
+  constructor(public firstName: string,
+              public secondName: string,
+              // tslint:disable-next-line:variable-name
+              public number: string) {}
+}
+const phoneContacts: IPhoneContact[] = [
+  {firstName: 'Ira', secondName: 'Tytar', number: '041414113424' },
+  {firstName: 'Vasylyna', secondName: 'Vrublevska', number: '3805113' },
+  {firstName: 'Alejandro', secondName: 'Del Rio Albrechet', number: '065131313' },
+  {firstName: 'Petro', secondName: 'Petriv', number: '089933144' },
+  {firstName: 'Petya', secondName: 'Zhuk', number: '0631111111' },
+];
 
 @Component({
   selector: 'app-root',
@@ -6,5 +29,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'phoneBook';
+  newContactFirstName: string;
+  newContactLastName: string;
+  newContactNumber: string;
+  closeResult = '';
+  displayedColumns: string[] = ['firstName', 'secondName', 'number','edit','delete'];
+  dataSource = new MatTableDataSource(phoneContacts);
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+  constructor(private modalService: NgbModal) {}
+  ngOnInit() {
+    this.dataSource.sort = this.sort;
+  }
+  addElement(){
+    const newElement = new PhoneContact(this.newContactFirstName, this.newContactFirstName , this.newContactNumber);
+    phoneContacts.push(newElement);
+    this.dataSource = new MatTableDataSource(phoneContacts);
+    this.dataSource.sort = this.sort;
+  }
+  saveBtnEventHandler(modal){
+    if (this.newContactFirstName && this.newContactLastName && this.newContactNumber){
+      this.addElement();
+      this.resetForm();
+      modal.close('Save click');
+    }
+  }
+  resetForm(){
+    this.newContactNumber = '';
+    this.newContactFirstName = '';
+    this.newContactLastName = '';
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatTableModule} from '@angular/material/table';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSortModule} from '@angular/material/sort';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {FormsModule} from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatSortModule,
+    NgbModule,
+    FormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
